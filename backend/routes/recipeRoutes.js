a180dd95a480dac2a61699c2a222bee19a8c218f
Marisetti Manikanta Sai <manikantasaimarisetti04@gmail.com>
2026-09-12 21:14:19 +0530

    Delete backend directory

--- backend/routes/recipeRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addRecipe } from "../controllers/recipeController.js";
-import authMiddleware from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-
-router.post("/", authMiddleware, addRecipe);
-
-export default router;
